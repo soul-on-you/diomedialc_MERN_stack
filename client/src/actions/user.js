3b2f6fea195933f1ecbea70777b12c3d4b0d3589
brendan <superden081@gmail.com>
2022-05-01 10:17:42 +0300

    added auth reducers for redux, auto auth for user and dispatches for redux

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { setUser } from "../reducers/userReducer";
 
 export const registration = async (email, password) => {
   try {
@@ -10,4 +11,41 @@ export const registration = async (email, password) => {
   } catch (err) {
     alert(err.response.data.message);
   }
+};
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response.data);
+      dispatch(setUser(response.data));
+      localStorage.setItem("token", response.data.token);
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
+};
+
+export const auth = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/auth/auth", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      });
+
+      console.log(response.data);
+
+      dispatch(setUser(response.data));
+      localStorage.setItem("token", response.data.token);
+    } catch (e) {
+      alert(e.response.data.message);
+      localStorage.removeItem("token");
+    }
+  };
 };
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,16 +3,31 @@ import Navbar from "./navbar/Navbar";
 import "./app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registration from "./registration/Registration";
+import Login from "./authorithation/Authorization";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { auth } from "../actions/user";
 
 function App() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(auth());
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar />
         <div className="wrap">
-          <Routes>
-            <Route path="/registration" element={<Registration />} />
-          </Routes>
+          {!isAuth && (
+            <Routes>
+              <Route path="/registration" element={<Registration />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          )}
         </div>
       </div>
     </BrowserRouter>
