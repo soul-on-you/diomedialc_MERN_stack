8fb996f8cea958492ea0c4aa24b3c6edc12b4437
brendan <superden081@gmail.com>
2022-05-01 10:15:54 +0300

    added auth react components

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,19 +1,44 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import "./navbar.css";
 import Logo from "../../assets/img/navbar-logo.svg";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { logout } from "../../reducers/userReducer";
+import { useDispatch } from "react-redux";
+
 const Navbar = () => {
+  const isAuth = useSelector((state) => state.user.isAuth);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="navbar">
       <div className="container">
         <img src={Logo} alt="" className="navbar__logo" />
         <div className="navbar__header">MERN CLOUD</div>
-        <div className="navbar__login">
-          <NavLink to="/login">Войти</NavLink>
-        </div>
-        <div className="navbar__registration">
-          <NavLink to="/registration">Регистрация</NavLink>
-        </div>
+        {!isAuth && (
+          <div className="navbar__login">
+            <NavLink to="/login">Войти</NavLink>
+          </div>
+        )}
+
+        {!isAuth && (
+          <div className="navbar__registration">
+            <NavLink to="/registration">Регистрация</NavLink>
+          </div>
+        )}
+
+        {isAuth && (
+          <div
+            className="navbar__login"
+            onClick={() => {
+              dispatch(logout());
+            }}
+          >
+            Выход
+          </div>
+        )}
       </div>
     </div>
   );
